64371e421f854d8a91d73c18de76635dc56aeead
manojkumarsanam <54481528+manojkumarsanam@users.noreply.github.com>
2021-02-25 21:28:24 +0530

    Create Experiment11.java

---
+++ Labexperiments/Experiment11.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+class Thread1 extends Thread{
+	private String name;
+
+	public Thread1(String name) {
+		this.name = name;
+	}
+	public void run() {
+		Random rand = new Random();
+		try {
+			while(true) {
+				int val = rand.nextInt(10)+1;
+				System.out.println("Input value "+val);
+				if(val%2 == 0) {
+						new Thread2("Thread2",val).start();
+				}
+				else {
+						new Thread3("Thread3",val).start();
+				}
+				Thread.sleep(1000);
+			}
+		}
+		catch(InterruptedException ie) {
+			ie.printStackTrace();
+		}
+	}	
+}
+
+class Thread2 extends Thread{
+	public String name;
+	private int value;
+
+	public Thread2(String name,int value) {
+		super();
+		this.name = name;
+		this.value = value;
+	}
+	public void run() {
+		int result = value * value;
+		//int result = (int)Math.pow(value, 2);
+		System.out.println(result);
+	}
+}
+class Thread3 extends Thread{
+	public String name;
+	private int value;
+
+	public Thread3(String name,int value) {
+		super();
+		this.name = name;
+		this.value = value;
+	}
+	public void run() {
+		int result = value * value * value;
+		//int result = (int)Math.pow(value, 3);
+		System.out.println(result);
+	}
+}
+public class Experiment11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Thread1("Thread1").start();
+	}
+
+}
